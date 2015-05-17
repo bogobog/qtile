4d63ad0dc360ebb9cb87aaddc2819a4e93b413cf
Ken Reeves <ken@apelint.com>
2015-05-17 04:48:27 -0700

    * Added DropDownGroupBox class.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,9 @@ from .command_text_box import *
 from .drop_down_box import *
 from .menu_box import *
 from .notification_box import *
+from .drop_down_group_box import *
+from .smaller_monad_tall import *
 
-__all__ = [ 'black_sep', 'command_text_box', 'drop_down_box', 'menu_box', 'notification_box' ]
+__all__ = [ 'black_sep', 'command_text_box', 'drop_down_box', 'menu_box', 'notification_box', 'drop_down_group_box', 'smaller_monad_tall' ]
 
 # vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab
---
+++ drop_down_group_box.py
@@ -0,0 +1,77 @@
+
+import math
+
+from libqtile.widget import GroupBox
+from .drop_down_box import DropDownBoxChild
+
+__all__ = [ 'DropDownGroupBox', ]
+
+class DropDownGroupBoxChild( DropDownBoxChild ):
+
+    def _configure( self, qtile, screen ):
+        DropDownBoxChild._configure( self, qtile, screen )
+
+        self.window.handle_ButtonRelease = self.handle_ButtonRelease
+
+    def getWindowItem( self, x, y ):
+        per_item_size = self.height / len( self.parent.active_group_windows )
+        item_index = int( math.floor( float( y ) / per_item_size ) )
+
+        return self.parent.active_group_windows[ item_index ]
+
+    def handle_ButtonRelease( self, e ):
+
+        if e.detail == 1:
+            self.parent.bar.screen.setGroup( self.parent.active_group )
+            window = self.getWindowItem( e.event_x, e.event_y )
+
+            window.group.focus( window, False )
+            if window.floating:
+                window.cmd_bring_to_front()
+
+        DropDownBoxChild.handle_ButtonRelease( self, e )
+
+class DropDownGroupBox( GroupBox ):
+
+    def __init__( self, *args, **kwargs ):
+        GroupBox.__init__( self, *args, **kwargs )
+
+        self.active_group_windows = []
+        self.active_group = None
+
+    def button_press( self, x, y, button ):
+        if not button == 3:
+            GroupBox.button_press( self, x, y, button )
+
+    def button_release( self, x, y, button ):
+        if button == 3:
+            clicked_group = self.get_clicked_group( x, y )
+
+            if not len( clicked_group.windows ) or clicked_group == self.active_group:
+                self.active_group_windows = []
+                self.active_group = None
+                self.child.window.hide()
+                return
+
+            self.active_group_windows = list( clicked_group.windows )
+            self.child.text = '\n'.join( list( win.name.encode( 'ascii', 'ignore' ) for win in self.active_group_windows ) )
+            self.child.window.unhide()
+
+            self.active_group = clicked_group
+        else:
+            self.child.window.hide()
+            GroupBox.button_release( self, x, y, button )
+
+    def _configure( self, qtile, screen ):
+        GroupBox._configure( self, qtile, screen )    
+    
+        self.borderwidth = 0
+        self.padding_y = 0
+        self.margin_y = -2
+
+        self.child = DropDownGroupBoxChild( self )
+        self.child._configure( qtile, screen )
+        self.child.text = ''
+
+
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab
