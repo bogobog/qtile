20b60b19990d5a7106dbfbb15003f7ee90477139
Ken Reeves <ken@apelint.com>
2015-05-17 04:47:57 -0700

    * Added monadtall layout with smaller shrink ratio.

---
+++ smaller_monad_tall.py
@@ -0,0 +1,9 @@
+
+from libqtile.layout import MonadTall
+
+class SmallerMonadTall( MonadTall ):
+
+    _min_ratio = .15
+    _max_ratio = .85
+
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab
