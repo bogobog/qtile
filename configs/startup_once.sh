67d70151312d9145966439c0b53a13a41b4f6f2d
Ken Reeves <ken@apelint.com>
2015-05-20 22:30:23 +0000

    * Added notification-daemon to startup_once.

--- configs/startup_once.sh
+++ configs/startup_once.sh
@@ -2,8 +2,10 @@
 
 if [[ -d "${HOME}/.fonts" ]]; then
     for d in ${HOME}/.fonts/*; do echo $d; xset fp+ ${d}; done;
+    xset fp rehash
 fi
 
+/usr/lib/notification-daemon/notification-daemon &
 ${HOME}/bin/qtile/agent.py &
 xsetroot -solid '#939393' &
 xscreensaver -nosplash &
