31418dfebf60fba8b4cd061436a505656dceb831
Ken Reeves <ken@apelint.com>
2015-05-20 15:13:55 -0700

    * Added font rehashing to startup_once.

--- configs/startup_once.sh
+++ configs/startup_once.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+if [[ -d "${HOME}/.fonts" ]]; then
+    for d in ${HOME}/.fonts/*; do echo $d; xset fp+ ${d}; done;
+fi
+
 ${HOME}/bin/qtile/agent.py &
 xsetroot -solid '#939393' &
 xscreensaver -nosplash &
 dropbox start &
 pidgin &
+
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab
