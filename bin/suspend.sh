1066aa4c2ca9a08629d03fd34fa8904d55cfa921
Ken Reeves <ken@apelint.com>
2015-05-21 02:35:11 +0000

    * Added platform specific suspend scripts.

--- bin/suspend.sh
+++ bin/suspend.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-
-# before
-xscreensaver-command -lock
-pkill -f keepass
-
-sleep 2
-sudo systemctl suspend
-
-# after
-/home/judar/bin/qtile/update_calendar.sh
-/home/judar/bin/qtile/update_time.sh
-/home/judar/bin/qtile/update_volume.sh
+configs/suspend.sh.work_laptop
