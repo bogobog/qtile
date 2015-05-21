a6be2b3d77eaf7a3c678150ba96ae693d38bc94a
Ken Reeves <ken@apelint.com>
2015-05-21 13:54:20 -0700

    * Added more intelligent multi-device support to the network status script.

--- bin/update_network_status.sh
+++ bin/update_network_status.sh
@@ -3,8 +3,8 @@
 global_status=$( nm-tool | grep '^State' | awk '{ print $2 }' )
 
 if [ "${global_status}" = 'connected' ]; then
-    current_device=$( nm-tool | grep 'Device' | awk '{ print $3 }' )
-    output="<span bgcolor=\"darkgreen\">${current_device}</span>"
+    current_devices=$( nm-tool | grep -E ' (Device|State)' | grep -B1 connected | grep Device | awk '{ print $3 }' | xargs )
+    output="<span bgcolor=\"darkgreen\">${current_devices}</span>"
 else
     output="<span bgcolor=\"red\">Down</span>"
 fi
