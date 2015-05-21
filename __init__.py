12676bf6230830f4d21418557f0c298234b43b59
Ken Reeves <ken@apelint.com>
2015-05-21 12:42:50 -0700

    * Removed notification_box widget in preference of gnomes notification-daemon.

--- __init__.py
+++ __init__.py
@@ -3,10 +3,9 @@ from .black_sep import *
 from .command_text_box import *
 from .drop_down_box import *
 from .menu_box import *
-from .notification_box import *
 from .drop_down_group_box import *
 from .smaller_monad_tall import *
 
-__all__ = [ 'black_sep', 'command_text_box', 'drop_down_box', 'menu_box', 'notification_box', 'drop_down_group_box', 'smaller_monad_tall' ]
+__all__ = [ 'black_sep', 'command_text_box', 'drop_down_box', 'menu_box', 'drop_down_group_box', 'smaller_monad_tall' ]
 
 # vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab
